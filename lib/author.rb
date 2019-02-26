acf5859c4cccdb3078038614927a355b511ed3ec
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-26 04:02:53 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    @posts = []
+  end 
+  
+  def add_post(new_post)
+    @posts << new_post
+    new_post.author = self
+  end
+  
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    @posts << new_post 
+    new_post.author = self 
+  end 
+  
+  def self.post_count
+    post_titles = @post.collect {|x| x.name}
+    post_titles.length
+  end
+  
+  def posts
+    @posts 
+  end
+  
+end 
